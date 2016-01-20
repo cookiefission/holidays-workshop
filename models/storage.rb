d11c9276af6d330dc26432588b7a23f05c18cccc
Sean Kenny <sean.kenny@hotmail.com>
2016-01-20 07:18:47 +0000

    Refactor holidays storage
    This improves readability by removing interop between class and instance
    methods, instead preferring instantiation early on.

--- models/storage.rb
+++ models/storage.rb
@@ -1,23 +1,34 @@
 require 'json'
 
 class Storage
+
+  attr_reader :criteria
+  private :criteria
+
+  def initialize(criteria)
+    @criteria = criteria
+  end
+
   def self.find(criteria)
-    holidays.select do |holiday|
-      holiday['region'].downcase.include? criteria[:q].to_s.downcase
-    end
+    new(criteria).find
   end
 
-  def self.holidays
-    new.load_holidays
-  end
-
-  def load_holidays
-    JSON.parse(File.read(db_file))
+  def find
+    holidays.select { |holiday| holiday['region'].downcase.include? query }
   end
 
   private
 
+  def query
+    criteria[:q].to_s.downcase
+  end
+
+  def holidays
+    JSON.parse(File.read(db_file))
+  end
+
   def db_file
     './db/holidays.json'
   end
+
 end
