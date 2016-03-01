5354a7b326e319465c700dc40878968cd3174ff9
Sean Holden <sean.holden@onthebeach.co.uk>
2016-03-01 13:09:45 +0000

    Added search function. Also cucumber and specs

--- spec/models/storage_spec.rb
+++ spec/models/storage_spec.rb
@@ -9,5 +9,40 @@ describe Storage do
         expect(holidays.count).to eq(12)
       end
     end
+
+    context 'when given some criteria' do
+      let(:criteria) {
+        { 'q' => 'algarve' }
+      }
+
+      it 'returns holiday that matches "algarve"' do
+        expect(holidays.count).to eq(1)
+      end
+
+      context 'criteria is partial match' do
+        let(:criteria) {
+          { 'q' => 'cos' }
+        }
+        let(:result_1) {
+          {"region"=>"Costa Brava", "price"=>700}
+        }
+        let(:result_2) {
+          {"region"=>"Costa Del Sol", "price"=>800}
+        }
+        let(:results) {
+          holidays.find.first(2)
+        }
+
+
+        it 'returns correct number of results' do
+          expect(holidays.count).to eq(2)
+        end
+
+        it 'returns correct result data' do
+          expect(results.first).to eql(result_1)
+          expect(results.last).to eql(result_2)
+        end
+      end
+    end
   end
 end
